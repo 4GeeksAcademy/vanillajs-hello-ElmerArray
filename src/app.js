3972fa539275df9e14d06aef5597eb651b5c3eb1
Elmer <elmerjosuesid@gmail.com>
2026-04-16 04:34:36 +0000

    entrega

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const listaUl = document.querySelector('#lista-dominios');
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  let domains = [".com" ,".net",".us",".io", ".es"]
+
+  let combinaciones = []
+
+  
+  for (let p of pronoun) {
+    for (let ad of adj) {
+      for (let no of noun){
+        for (let dom of domains) {
+            combinaciones.push(`${p}${ad}${no}${dom}`)
+
+
+
+            let li = document.createElement('li');
+
+            li.classList.add('list-group-item')
+
+            li.innerText = combinaciones;
+
+            listaUl.appendChild(li);
+        }
+      } 
+    }
+  }
+   //write your code here
 };
